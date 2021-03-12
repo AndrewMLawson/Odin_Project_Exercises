de85f607b5dabd8a7246be6cdd1729e0d1e2c7be
Andrew Lawson <andrewlawson234@gmail.com>
2021-03-11 17:13:20 -0800

    Complete 3.1

---
+++ JavaScript Basics/Fundamentals_3.1.js
@@ -0,0 +1,13 @@
+//Write a function called add7 that takes one number and returns that number + 7.
+
+let test1 = 0
+let test2 = 3
+let test3 = -2
+
+
+
+function add7(num){
+    return num + 7
+}
+
+console.log(add7(test3))
